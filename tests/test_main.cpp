2b110dcf2dc0dcf00e9dc4518c31c4c0e69019cc
NickLev17 <LevenkovNick17@yandex.ru>
2026-06-28 13:02:03 +0300

    Update test_main.cpp

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -4,77 +4,161 @@
 #include <memory>
 #include <unordered_map>
 #include <math.h>
+#include <stdexcept>
 #include "../include/Interfaces/IMovingObject.h"
 #include "../include/Interfaces/IRotatingObject.h"
 #include "../include/Interfaces/IFuelObject.h"
 #include "../include/Interfaces/IMovingObject.h"
 #include "../include/Interfaces/IRotatingObject.h"
-// #include "../include/Interfaces/IBurnFuelObject.h"
+#include "../include/Interfaces/IBurnFuelObject.h"
 #include "../include/Commands/MoveCommand.h"
 #include "../include/Commands/RotateCommand.h"
 #include "../include/Commands/CheckFuelCommand.h"
 #include "../include/Commands/BurnFuelCommand.h"
 #include "../include/SpaceShip.h"
 #include "../include/Commands/MacroCommand.h"
+#include<gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using ::testing::Return;
+using ::testing::_;
+using namespace testing;
 using namespace std;
 
-int main()
+class MockBurnFuelObject : public IBurnFuelObject 
 {
-    std::cout << "Hello, World!" << std::endl;
+public:
+MOCK_CONST_METHOD0(getFuel, double());
+MOCK_METHOD1(setFuel, void(double));
+};
 
-    // 1. Создаем один объект SpaceShip
-    SpaceShip ship_for_commands(20);
-    ship_for_commands.setDirection(11);
-    ship_for_commands.setAngularVelocity(50);
-    ship_for_commands.setDirectionsNumber(360);
-    ship_for_commands.setStepFuel(5);
-    Point p(15, 24);
-    ship_for_commands.setPosition(p);
+class MockFuelObject : public IFuelObject 
+{
+public:
+    MOCK_CONST_METHOD0(getFuel, double());
+    MOCK_CONST_METHOD0(getStepFuel, double());
+    MOCK_METHOD1(setFuel, void(double));
+    MOCK_METHOD1(setStepFuel, void(double));
+};
 
-  
+TEST(BurnFuelTest, GetAndSetFuelSequence) 
+{
+MockBurnFuelObject mock;
 
-    // vec.push_back(std::make_unique<CheckFuelCommand>(ship_for_commands));
-    // vec.push_back(std::make_unique<MoveCommand>(ship_for_commands));
-    // vec.push_back(std::make_unique<BurnFuelCommand>(ship_for_commands));
-    // vec.push_back(std::make_unique<RotateCommand>(ship_for_commands));
+EXPECT_CALL(mock, getFuel())
+.WillOnce(testing::Return(10.0))
+.WillRepeatedly(testing::Return(7.5));
 
-    std::vector<std::unique_ptr<ICommand>> vec;
-    vec.push_back(std::make_unique<CheckFuelCommand>(ship_for_commands));
-    vec.push_back(std::make_unique<MoveCommand>(ship_for_commands));
-    vec.push_back(std::make_unique<BurnFuelCommand>(ship_for_commands));
+EXPECT_CALL(mock, setFuel(testing::DoubleEq(7.5)));
+
+EXPECT_DOUBLE_EQ(mock.getFuel(), 10.0);
+
+mock.setFuel(7.5);
+
+EXPECT_DOUBLE_EQ(mock.getFuel(), 7.5);
+}
+
+TEST(FuelObjectTest, StepFuelBehavior) 
+{
+MockFuelObject mock;
+
+EXPECT_CALL(mock, getFuel())
+.WillOnce(testing::Return(5.0))
+.WillRepeatedly(testing::Return(4.5));
+
+EXPECT_CALL(mock, getStepFuel())
+.WillOnce(testing::Return(0.5))
+.WillRepeatedly(testing::Return(0.5));
+
+EXPECT_CALL(mock, setStepFuel(testing::DoubleEq(0.5)));
+EXPECT_CALL(mock, setFuel(testing::DoubleEq(4.5)));
+
+EXPECT_DOUBLE_EQ(mock.getFuel(), 5.0);
+
+mock.setStepFuel(0.5);
+
+mock.setFuel(4.5);
+
+EXPECT_DOUBLE_EQ(mock.getStepFuel(), 0.5);
+EXPECT_DOUBLE_EQ(mock.getFuel(), 4.5);
+}
+
+TEST(CheckFuelCommandTest, ConstructorReadsFuel)
+{
+    MockFuelObject obj;
+    EXPECT_CALL(obj, getFuel())
+        .WillOnce(Return(10.0));
+
+    CheckFuelCommand cmd(obj);
+}
 
 
-    std::vector<std::unique_ptr<ICommand>> vec2;
-    vec2.push_back(std::make_unique<CheckFuelCommand>(ship_for_commands));
-    vec2.push_back(std::make_unique<RotateCommand>(ship_for_commands));
-    vec2.push_back(std::make_unique<BurnFuelCommand>(ship_for_commands));
+TEST(CheckFuelCommandTest, ExecuteThrowsWhenFuelIsNotEnough)
+{
+    MockFuelObject obj;
 
-       std::vector<std::unique_ptr<ICommand>> vec3;
-    vec3.push_back(std::make_unique<CheckFuelCommand>(ship_for_commands));
-    vec3.push_back(std::make_unique<RotateCommand>(ship_for_commands));
-    vec3.push_back(std::make_unique<BurnFuelCommand>(ship_for_commands));
+    EXPECT_CALL(obj, getFuel())
+        .WillOnce(Return(1.0))
+        .WillOnce(Return(1.0));
 
-     std::vector<std::unique_ptr<ICommand>> vec4;
-    vec4.push_back(std::make_unique<CheckFuelCommand>(ship_for_commands));
-    vec4.push_back(std::make_unique<RotateCommand>(ship_for_commands));
-    vec4.push_back(std::make_unique<BurnFuelCommand>(ship_for_commands));
+    EXPECT_CALL(obj, getStepFuel())
+        .WillOnce(Return(2.5));
 
-     std::vector<std::unique_ptr<ICommand>> vec5;
-    vec5.push_back(std::make_unique<CheckFuelCommand>(ship_for_commands));
-    vec5.push_back(std::make_unique<RotateCommand>(ship_for_commands));
-    vec5.push_back(std::make_unique<BurnFuelCommand>(ship_for_commands));
+    CheckFuelCommand cmd(obj);
+    EXPECT_THROW(cmd.execute(), const char*);
+}
 
-    std::vector<std::unique_ptr<ICommand>> vec_all;
-    vec_all.push_back(std::make_unique<MacroCommand>(std::move(vec))); 
-    vec_all.push_back(std::make_unique<MacroCommand>(std::move(vec2)));
-    vec_all.push_back(std::make_unique<MacroCommand>(std::move(vec3)));
-    vec_all.push_back(std::make_unique<MacroCommand>(std::move(vec4)));
-    vec_all.push_back(std::make_unique<MacroCommand>(std::move(vec5)));
+class MockCommand : public ICommand 
+{
+public:
 
-    MacroCommand macro(std::move(vec_all)); 
- vector<unique_ptr<ICommand>>
-    macro.execute();
+    MOCK_METHOD(void, execute, (), (override));
+};
 
-    return 0;
+
+TEST(MacroCommandTest, ExecuteWithNoCommandsDoesNotThrow) 
+{
+    std::vector<std::unique_ptr<ICommand>> emptyCommands;
+
+    MacroCommand cmd(std::move(emptyCommands));
+
+    EXPECT_NO_THROW(cmd.execute());
+}
+
+TEST(MacroCommandTest, ExecuteCallsExecuteOnSingleCommand) 
+{
+    std::vector<std::unique_ptr<ICommand>> commands;
+    commands.reserve(1); 
+    commands.push_back(std::make_unique<MockCommand>());
+
+    EXPECT_CALL(static_cast<MockCommand&>(*commands[0]), execute()).Times(1);
+
+    MacroCommand cmd(std::move(commands));
+    cmd.execute();
+}
+
+TEST(MacroCommandTest, ExecuteCallsExecuteOnAllCommandsInSequence) 
+{
+    std::vector<std::unique_ptr<ICommand>> commands;
+    commands.reserve(3);
+    commands.push_back(std::make_unique<MockCommand>());
+    commands.push_back(std::make_unique<MockCommand>());
+    commands.push_back(std::make_unique<MockCommand>());
+
+    ::testing::InSequence s;
+
+    EXPECT_CALL(static_cast<MockCommand&>(*commands[0]), execute()).Times(1);
+    EXPECT_CALL(static_cast<MockCommand&>(*commands[1]), execute()).Times(1);
+    EXPECT_CALL(static_cast<MockCommand&>(*commands[2]), execute()).Times(1);
+
+    MacroCommand cmd(std::move(commands));
+    cmd.execute();
+}
+
+
+int main(int argc, char **argv) {
+::testing::InitGoogleMock(&argc, argv);
+::testing::InitGoogleTest(&argc, argv);
+return RUN_ALL_TESTS();
 }
 
